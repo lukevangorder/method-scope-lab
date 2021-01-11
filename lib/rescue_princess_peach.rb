9333d0501343c9f8a60e9bcd7f6aaa0dee6a43f8
lukevangorder <lukevangorder@gmail.com>
2021-01-11 15:15:43 -0500

    commit tree a08ad48bc78fbf70edf78fcdd54d6b5409699ea2

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,10 @@
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
+  return status
 end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
+status = rescue_princess_peach
+puts status
